ab72e27db88ae122ea25bed51c608b4da108d0f9
JuuseK <juuse.kuuppo@gmail.com>
2023-11-25 22:54:56 +0200

    tweaking

--- drill-and-practice/deps.js
+++ drill-and-practice/deps.js
@@ -5,6 +5,7 @@ export {
   send,
 } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 import postgres from "https://deno.land/x/postgresjs@v3.3.5/mod.js";
+export { serve } from "https://deno.land/std@0.202.0/http/server.ts";
 export { postgres };
 export { Session } from "https://deno.land/x/oak_sessions@v4.1.9/mod.ts";
 export * as bcrypt from "https://deno.land/x/bcrypt@v0.4.1/mod.ts";
